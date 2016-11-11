e757f25fa77906793ff556ee82999a0b323e3b31
boris-ns <b.sulicenko@gmail.com>
2016-11-11 23:39:57 +0100

    Fixed movement bug and added ESC command for exiting game

--- src/engine/KeyInput.java
+++ src/engine/KeyInput.java
@@ -14,9 +14,14 @@ public class KeyInput extends KeyAdapter
     Game game;
     Integer key;
 
+    private boolean keyDown[] = new boolean[4]; // 0-up 1-down 2-right 3-left
+
     public KeyInput(Game g)
     {
         game = g;
+
+        for(int i = 0; i < keyDown.length; ++i)
+            keyDown[i] = false;
     }
 
 
@@ -25,10 +30,14 @@ public class KeyInput extends KeyAdapter
         key = e.getKeyCode();
         Hero hero = game.getHero();
 
-        if (key == KeyEvent.VK_LEFT) hero.setVelX(-5);
-        if (key == KeyEvent.VK_RIGHT) hero.setVelX(5);
-        if (key == KeyEvent.VK_UP) hero.setVelY(-5);
-        if (key == KeyEvent.VK_DOWN) hero.setVelY(5);
+        if (key == KeyEvent.VK_LEFT) { hero.setVelX(-5); keyDown[3] = true; }
+        if (key == KeyEvent.VK_RIGHT) { hero.setVelX(5); keyDown[2] = true;}
+        if (key == KeyEvent.VK_UP) { hero.setVelY(-5); keyDown[0] = true; }
+        if (key == KeyEvent.VK_DOWN) { hero.setVelY(5); keyDown[1] = true; }
+
+
+        if(key == KeyEvent.VK_ESCAPE)
+            System.exit(1);
     }
 
 
@@ -37,10 +46,16 @@ public class KeyInput extends KeyAdapter
         key = e.getKeyCode();
         Hero hero = game.getHero();
 
-        if (key == KeyEvent.VK_LEFT) hero.setVelX(0);
-        if (key == KeyEvent.VK_RIGHT) hero.setVelX(0);
-        if (key == KeyEvent.VK_UP) hero.setVelY(0);
-        if (key == KeyEvent.VK_DOWN) hero.setVelY(0);
+        if (key == KeyEvent.VK_LEFT) keyDown[3] = false;
+        if (key == KeyEvent.VK_RIGHT) keyDown[2] = false;
+        if (key == KeyEvent.VK_UP) keyDown[0] = false;
+        if (key == KeyEvent.VK_DOWN) keyDown[1] = false;
+
+        //vertical movement
+        if(!keyDown[0] && !keyDown[1]) hero.setVelY(0);
+
+        //horizontal movement
+        if(!keyDown[2] && !keyDown[3]) hero.setVelX(0);
 
         key = null;
     }
